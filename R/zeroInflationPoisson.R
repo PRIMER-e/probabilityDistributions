099c65dc986b40708387cb726fb12a6c2139c3ab
andrew-primer-e <andrew@primer-e.co.nz>
2021-11-24 15:35:07 +1300

    Fix a typo in dzip.

--- R/zeroInflationPoisson.R
+++ R/zeroInflationPoisson.R
@@ -1,6 +1,6 @@
 dzip <- function(x, lambda, pi, log = FALSE) {
   if(any(pi < 0 | pi > 1)) {
-    stop(glue::glue("argument pi = contains values that lie outside the  interval [0, 1]"))
+    stop(glue::glue("argument pi contains values that lie outside the  interval [0, 1]"))
   }
 
   probabilityMass = pi * (x == 0) + (1 - pi) * stats::dpois(x, lambda)
