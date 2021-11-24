3719a221f30a867aa3a53e2bba2bdbdacfe964bc
andrew-primer-e <andrew@primer-e.co.nz>
2021-11-24 14:50:42 +1300

    Tweak the dpois_stan documentation.

--- R/poisson.R
+++ R/poisson.R
@@ -1,15 +1,15 @@
 
 #' Stan code for the Poisson Log Probability Density
 #'
-#' Stan already has built-in support for the Poisson distribution via the
-#' poisson_lpmf function.
-#' Therefore, this function just returns some comments directing people
-#' to use that.
+#' Stan has a built-in function which calculates the log probability density
+#' of the the Poisson distribution:
+#' \code{  real poisson_lpmf(ints n | reals lambda)}
 #'
 #' @return A text string containing Stan code.
 #' @export
 dpois_stan <- function() {
-  "// The Poisson log probability density is built-in to Stan.
-// It's available via the function poisson_lpmf( n | lambda).
+  "// Stan has a built-in function which calculates the log probability density
+// of  the Poisson distribution:
+//   real poisson_lpmf(ints n | reals lambda)
 "
 }
