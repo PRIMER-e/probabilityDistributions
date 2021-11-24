4f2ec24556becac477fb815dd54b466bd4d1dd7e
andrew-primer-e <andrew@primer-e.co.nz>
2021-11-24 15:32:32 +1300

    Fix some bugs in dzip.

--- R/zeroInflationPoisson.R
+++ R/zeroInflationPoisson.R
@@ -1,9 +1,9 @@
 dzip <- function(x, lambda, pi, log = FALSE) {
-  if(pi < 0 || pi > 1) {
-    stop(glue::glue("argument pi = {pi} is not in the interval [0, 1]"))
+  if(any(pi < 0 | pi > 1)) {
+    stop(glue::glue("argument pi = contains values that lie outside the  interval [0, 1]"))
   }
 
-  probabilityMass = pi * (x == 0) + (1 - pi) * dpois(x, lambda)
+  probabilityMass = pi * (x == 0) + (1 - pi) * stats::dpois(x, lambda)
 
   if (log) {
     return(log(probabilityMass))
