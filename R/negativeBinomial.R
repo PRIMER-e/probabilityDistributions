c40a52597d4644783cddddcda1d0245a615cdc3b
andrew-primer-e <andrew@primer-e.co.nz>
2021-11-24 14:53:51 +1300

    Add code for the Stan negative binomial density.

---
+++ R/negativeBinomial.R
@@ -0,0 +1,20 @@
+#' Stan code for the Negative Binomial Log Probability Density
+#'
+#' Stan has built-in functions for calculating the log probability density
+#' of the negative binomial distribution:
+#' \preformatted{
+#' real neg_binomial_lpmf(ints n | reals alpha, reals beta)
+#' real neg_binomial_2_lpmf(ints y | reals mu, reals phi)
+#' real neg_binomial_2_log_lpmf(ints y | reals eta, reals phi)
+#' }
+#'
+#' @return A text string containing Stan code.
+#' @export
+dnbinom_stan <- function() {
+  "// Stan has built-in functions for calculating the log probability density
+// of the negative binomial distribution:
+// real neg_binomial_lpmf(ints n | reals alpha, reals beta)
+// real neg_binomial_2_lpmf(ints y | reals mu, reals phi)
+// real neg_binomial_2_log_lpmf(ints y | reals eta, reals phi)
+"
+}
