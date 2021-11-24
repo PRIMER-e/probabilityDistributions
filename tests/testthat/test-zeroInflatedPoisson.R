8bec70963d5d7284dc785543884e90456d8f945e
andrew-primer-e <andrew@primer-e.co.nz>
2021-11-24 15:35:17 +1300

    Add another test for dzip.

--- tests/testthat/test-zeroInflatedPoisson.R
+++ tests/testthat/test-zeroInflatedPoisson.R
@@ -1,27 +1,27 @@
 library(probabilityDistributions)
 
-test_that("zip(x, lambda, 0) == dpois(x, lambda, 0)", {
+test_that("dzip(x, lambda, 0) == dpois(x, lambda, 0)", {
   x = sample.int(100)
   lambda = runif(100, 0, 100)
 
   expect_equal(dzip(x, lambda, 0), dpois(x, lambda))
 })
 
-test_that("zip(0, lambda, 1) == 1", {
+test_that("dzip(0, lambda, 1) == 1", {
   lambda = runif(100, 0, 100)
 
   expect_equal(dzip(0, lambda, 1), rep(1, 100))
 })
 
 
-test_that("zip(x, lambda, 1) == 0 when x > 0", {
+test_that("dzip(x, lambda, 1) == 0 when x > 0", {
   x = sample.int(100)
   lambda = runif(100, 0, 100)
 
   expect_equal(dzip(x, lambda, 1), rep(0, 100))
 })
 
-test_that("zip(x, lambda, pi) == (1 - pi) * dpois(x, lambda)  when x > 0", {
+test_that("dzip(x, lambda, pi) == (1 - pi) * dpois(x, lambda)  when x > 0", {
   x = sample.int(100)
   lambda = runif(100, 0, 100)
   pi = runif(100, 0, 1)
@@ -29,17 +29,21 @@ test_that("zip(x, lambda, pi) == (1 - pi) * dpois(x, lambda)  when x > 0", {
   expect_equal(dzip(x, lambda, pi), (1 - pi) * dpois(x, lambda))
 })
 
-test_that("zip(0, lambda, pi) == pi + dpois(0, lambda)", {
+test_that("dzip(0, lambda, pi) == pi + dpois(0, lambda)", {
   lambda = runif(100, 0, 100)
   pi = runif(100, 0, 1)
 
   expect_equal(dzip(0, lambda, pi), pi + (1 - pi) * dpois(0, lambda))
 })
 
-test_that("zip(x, lambda, pi, log = TRUE) == log((1 - pi) * dpois(x, lambda))  when x > 0", {
+test_that("dzip(x, lambda, pi, log = TRUE) == log((1 - pi) * dpois(x, lambda))  when x > 0", {
   x = sample.int(100)
   lambda = runif(100, 0, 100)
   pi = runif(100, 0, 1)
 
   expect_equal(dzip(x, lambda, pi, log = TRUE), log((1 - pi) * dpois(x, lambda)))
+})
+
+test_that("dzip(x, lambda, 1.5) fails", {
+  expect_error(dzip(5, 7, 1.5))
 })
