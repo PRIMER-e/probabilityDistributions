ad048efe7f362c49acb6f68acf04049ea6e516f6
andrew-primer-e <andrew@primer-e.co.nz>
2021-11-29 16:23:38 +1300

    Tweak dzinbinom docs

--- R/zeroInflatedNegativeBinomial.R
+++ R/zeroInflatedNegativeBinomial.R
@@ -1,16 +1,15 @@
 #' Zero-inflated Negative Binomial Probability Mass
 #'
 #' @param x vector of (non-negative integer) values.
-#' @param size
-#' @param prob
-#' @param mu
-#' @param phi vector of over-dispersion values.
+#' @param size target for number of successful trials, or dispersion parameter (the shape parameter of the gamma mixing distribution). Must be strictly positive, need not be integer.
+#' @param prob probability of success in each trial. 0 < prob <= 1.
+#' @param mu alternative parametrization via mean: see ‘Details’.
 #' @param pi vector of (real lying in \[0, 1\]) zero-inflation parameters.
 #' @param log logical; if TRUE, probabilities, p, are given as log(p).
 #'
 #' @return The (log) probability mass at x, given lambda and pi.
 #' @export
-dzinbinom <- function(x, size, prob, mu, log = FALSE) {
+dzinbinom <- function(x, size, prob, mu, pi, log = FALSE) {
   if(any(pi < 0 | pi > 1)) {
     stop(glue::glue("argument pi contains values that lie outside the  interval [0, 1]"))
   }
