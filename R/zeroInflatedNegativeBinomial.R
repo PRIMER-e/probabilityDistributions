4245b5008ac09c96ffc4271fc833452cf09cf64c
andrew-primer-e <andrew@primer-e.co.nz>
2021-12-19 18:48:34 +1300

    Switch to the ecological parameterisation of NB.

--- R/zeroInflatedNegativeBinomial.R
+++ R/zeroInflatedNegativeBinomial.R
@@ -1,23 +1,21 @@
 #' Zero-inflated Negative Binomial Probability Mass
 #'
 #' @param x vector of (non-negative integer) values.
-#' @param size target for number of successful trials, or dispersion parameter (the shape parameter of the gamma mixing distribution). Must be strictly positive, need not be integer.
-#' @param prob probability of success in each trial. 0 < prob <= 1.
-#' @param mu alternative parametrization via mean: see ‘Details’.
+#' @param mu mean of the distribution.
+#' @param phi dispersion parameter.
 #' @param pi vector of (real lying in \[0, 1\]) zero-inflation parameters.
 #' @param log logical; if TRUE, probabilities, p, are given as log(p).
 #'
-#' @return The (log) probability mass at x, given lambda and pi.
+#' @return The (log) probability mass at x, given mu, phi and pi.
 #' @export
-dzinb <- function(x, size, prob, mu, pi, log = FALSE) {
+dzinb_2 <- function(x, mu, phi, pi, log = FALSE) {
   if(any(pi < 0 | pi > 1)) {
     stop(glue::glue("argument pi contains values that lie outside the  interval [0, 1]"))
   }
 
   probabilityMass = pi * (x == 0) +
                     (1 - pi) * stats::dnbinom(x = x,
-                                              size = size,
-                                              prob = prob,
+                                              size = phi,
                                               mu = mu)
 
   if (log) {
@@ -30,16 +28,15 @@ dzinb <- function(x, size, prob, mu, pi, log = FALSE) {
 #' Zero-inflated Negative Binomial Quantile Function
 #'
 #' @param p vector of quantiles.
-#' @param size target for number of successful trials, or dispersion parameter (the shape parameter of the gamma mixing distribution). Must be strictly positive, need not be integer.
-#' @param prob probability of success in each trial. 0 < prob <= 1.
-#' @param mu alternative parametrization via mean: see ‘Details’.
+#' @param mu mean of the distribution.
+#' @param phi dispersion parameter.
 #' @param pi vector of (in \eqn{[0, 1]}) zero-inflation parameters.
 #' @param lower.tail logical; if TRUE (default), probabilities are \eqn{P[X <= x]}, otherwise, \eqn{P[X > x]}.
 #' @param log.p logical; if TRUE, probabilities p. This doesn't affect pi.
 #'
 #' @return A vector of quantiles, each of which coincide with the respective probability in p.
 #' @export
-qzinb <- function(p, size, prob, mu, pi, lower.tail = TRUE, log.p = FALSE) {
+qzinb_2 <- function(p, mu, phi, pi, lower.tail = TRUE, log.p = FALSE) {
   if (length(lower.tail) != 1) {
     stop("lower.tail should be length 1.")
   }
@@ -64,7 +61,11 @@ qzinb <- function(p, size, prob, mu, pi, lower.tail = TRUE, log.p = FALSE) {
               p_linear,
               1 - p_linear)
 
-  q <- ifelse(p_lower < pi, 0, stats::qnbinom(p_lower - pi, size, prob, mu, lower.tail = TRUE, log.p = FALSE))
+  q <- ifelse(p_lower < pi, 0, stats::qnbinom(q = p_lower - pi,
+                                              size = phi,
+                                              mu = mu,
+                                              lower.tail = TRUE,
+                                              log.p = FALSE))
 
   q
 }
--- R/zeroInflatedNegativeBinomialLinked.R
+++ R/zeroInflatedNegativeBinomialLinked.R
@@ -1,28 +1,26 @@
 #' Zero-inflated Negative Binomial Linked Probability Mass
 #'
 #' @param x vector of (non-negative integer) values.
-#' @param size target for number of successful trials, or dispersion parameter (the shape parameter of the gamma mixing distribution). Must be strictly positive, need not be integer.
-#' @param prob probability of success in each trial. 0 < prob <= 1.
-#' @param mu alternative parametrization via mean: see ‘Details’.
+#' @param mu mean of the distribution.
+#' @param phi dispersion parameter.
 #' @param gamma0 The zero-inflation to mean link intercept.
 #' @param gamma1 The zero-inflation to mean link gradient.
 #' @param log logical; if TRUE, probabilities, p, are given as log(p).
 #'
 #' @return The (log) probability mass at x, given lambda and pi.
 #' @export
-dzinbl <- function(x, size, prob, mu, gamma0, gamma1, log = FALSE) {
+dzinbl_2 <- function(x, mu, phi, gamma0, gamma1, log = FALSE) {
 
   pi = stats::qlogis(gamma0 + gamma1 * mu)
 
-  dzinb(x, size, prob, mu, pi, log = log)
+  dzinb_2(x, mu, phi, pi, log = log)
 }
 
 #' Zero-inflated Negative Linked Binomial Quantile Function
 #'
 #' @param p vector of quantiles.
-#' @param size target for number of successful trials, or dispersion parameter (the shape parameter of the gamma mixing distribution). Must be strictly positive, need not be integer.
-#' @param prob probability of success in each trial. 0 < prob <= 1.
-#' @param mu alternative parametrization via mean: see ‘Details’.
+#' @param mu mean of the distribution.
+#' @param phi dispersion parameter.
 #' @param gamma0 The zero-inflation to mean link intercept.
 #' @param gamma1 The zero-inflation to mean link gradient.
 #' @param lower.tail logical; if TRUE (default), probabilities are \eqn{P[X <= x]}, otherwise, \eqn{P[X > x]}.
@@ -30,12 +28,10 @@ dzinbl <- function(x, size, prob, mu, gamma0, gamma1, log = FALSE) {
 #'
 #' @return A vector of quantiles, each of which coincide with the respective probability in p.
 #' @export
-qzinbl <- function(p, size, prob, mu, gamma0, gamma1, lower.tail = TRUE, log.p = FALSE) {
-  # TODO: Change everything to the mean / over-dispersion parameterisation,
-  #       as we always need the mean for the linkage.
+qzinbl_2 <- function(p, mu, phi, gamma0, gamma1, lower.tail = TRUE, log.p = FALSE) {
   pi = stats::qlogis(gamma0 + gamma1 * mu)
 
-  qzinb(p, size, prob, mu, pi, lower.tail = lower.tail, log.p = log.p)
+  qzinb_2(p, mu, phi, pi, lower.tail = lower.tail, log.p = log.p)
 }
 
 #' Zero-inflated Negative-binomial Linked Log Probability Mass Function Stan Code
